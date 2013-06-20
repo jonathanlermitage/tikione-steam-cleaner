81f8ec2a25acfff34c0cc4460495209c9281701b
jonathanlermitage <jonathan.lermitage@gmail.com>
2013-06-20 23:26:23 +0200

    set new version Signed-off-by:jonathanlermitage <jonathan.lermitage@gmail.com>

--- src/fr/tikione/steam/cleaner/Version.java
+++ src/fr/tikione/steam/cleaner/Version.java
@@ -4,9 +4,9 @@ public class Version {
 
     public static final int MAJOR = 2;
 
-    public static final int MINOR = 1;
+    public static final int MINOR = 2;
 
-    public static final int PATCH = 5;
+    public static final int PATCH = 0;
 
     public static final String VERSION = MAJOR + "." + MINOR + "." + PATCH;
 
