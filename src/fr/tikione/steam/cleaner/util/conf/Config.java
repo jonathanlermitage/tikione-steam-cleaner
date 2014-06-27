688c1db3edd0fda90e1ef8af825271fdab866a77
jonathanlermitage <jonathan.lermitage@gmail.com>
2014-06-27 17:05:30 +0200

    v2.3.1 release

--- src/fr/tikione/steam/cleaner/util/conf/Config.java
+++ src/fr/tikione/steam/cleaner/util/conf/Config.java
@@ -35,7 +35,7 @@ public class Config {
     /** INI configuration file section : UI settings. */
     private static final String CONFIG_MAIN_WINDOW_UI = "MAIN_WINDOW_UI";
 
-    /**  INI configuration file key : latest Window length. */
+    /** INI configuration file key : latest Window length. */
     private static final String CONFIG_MAIN_WINDOW_UI__SIZE_X = "latestLength";
 
     /** INI configuration file key : latest Window height. */
@@ -74,12 +74,6 @@ public class Config {
     /** INI configuration file key : check for updates at startup. */
     private static final String CONFIG_MISC__CHK_FOR_UPT_AT_STATUP = "checkForUpdatesAtStartup";
 
-    /** INI configuration file section : update center. */
-    private static final String CONFIG_UPDATECENTER = "UPDATE_CENTER";
-
-    /** INI configuration file key : the URL to check for new version.. */
-    private static final String CONFIG_UPDATECENTER__LATEST_VERSION_URL = "latestVersionUrl";
-
     /** Singleton handler. */
     private static final Config config;
 
@@ -95,7 +89,7 @@ public class Config {
         // Singleton creation.
         try {
             config = new Config();
-        } catch (IOException ex) {
+        } catch (IOException | InfinitiveLoopException ex) {
             Log.error(ex);
             throw new RuntimeException(ex);
         }
@@ -114,14 +108,14 @@ public class Config {
      * Load application configuration file. A default configuration file is created if necessary.
      *
      * @throws IOException if an I/O error occurs while retrieving the internal default configuration file, or while writing this default
-     *         configuration file.
+     * configuration file.
      */
     private Config()
-            throws IOException {
+            throws IOException, CharConversionException, InfinitiveLoopException {
         File backupConfigFile = new File("conf/backup/tikione-steam-cleaner_config.ini");
         File userprofile = new File(System.getenv("USERPROFILE") + "/.tikione/");
         userprofile.mkdirs();
-        configFile = new File(userprofile.getAbsolutePath() + "/tikione-steam-cleaner_config.ini");
+        configFile = new File(userprofile.getAbsolutePath() + "/tikione-steam-cleaner_config_rev231.ini");
         if (!configFile.exists()) {
             org.apache.commons.io.FileUtils.copyFile(backupConfigFile, configFile);
         }
@@ -129,6 +123,41 @@ public class Config {
         ini.getConfig().enableParseLineConcat(true);
         ini.getConfig().enableReadUnicodeEscConv(true);
         ini.load(configFile, Main.CONF_ENCODING);
+
+        // Patch for 2.3.1 revision: migrate previous profile to new one.
+        try {
+            File configFileP231 = new File(userprofile.getAbsolutePath() + "/tikione-steam-cleaner_config.ini");
+            if (configFileP231.exists()) {
+                Ini iniP231 = new Ini();
+                iniP231.getConfig().enableParseLineConcat(true);
+                iniP231.getConfig().enableReadUnicodeEscConv(true);
+                iniP231.load(configFileP231, Main.CONF_ENCODING);
+                ini.setKeyValue(
+                        CONFIG_STEAM_FOLDERS, CONFIG_STEAM_FOLDERS__LATEST_DIR,
+                        iniP231.getKeyValue(CONFIG_STEAM_FOLDERS, CONFIG_STEAM_FOLDERS__LATEST_DIR));
+                ini.setKeyValue(
+                        CONFIG_STEAM_FOLDERS, CONFIG_STEAM_FOLDERS__MAX_DEPTH,
+                        iniP231.getKeyValue(CONFIG_STEAM_FOLDERS, CONFIG_STEAM_FOLDERS__MAX_DEPTH));
+                ini.setKeyValue(
+                        CONFIG_LANG, CONFIG_LANG__SELECTION,
+                        iniP231.getKeyValue(CONFIG_LANG, CONFIG_LANG__SELECTION));
+                ini.setKeyValue(
+                        CONFIG_MAIN_WINDOW_UI, CONFIG_MAIN_WINDOW_UI__SIZE_X,
+                        iniP231.getKeyValue(CONFIG_MAIN_WINDOW_UI, CONFIG_MAIN_WINDOW_UI__SIZE_X));
+                ini.setKeyValue(
+                        CONFIG_MAIN_WINDOW_UI, CONFIG_MAIN_WINDOW_UI__SIZE_Y,
+                        iniP231.getKeyValue(CONFIG_MAIN_WINDOW_UI, CONFIG_MAIN_WINDOW_UI__SIZE_Y));
+                ini.setKeyValue(
+                        CONFIG_MAIN_WINDOW_UI, CONFIG_MAIN_WINDOW_UI__STATE,
+                        iniP231.getKeyValue(CONFIG_MAIN_WINDOW_UI, CONFIG_MAIN_WINDOW_UI__STATE));
+                ini.setKeyValue(
+                        CONFIG_MISC, CONFIG_MISC__CHK_FOR_UPT_AT_STATUP,
+                        iniP231.getKeyValue(CONFIG_MISC, CONFIG_MISC__CHK_FOR_UPT_AT_STATUP));
+                configFileP231.delete();
+            }
+        } catch (InfinitiveLoopException | IOException | IllegalArgumentException ex) {
+            Log.error(ex);
+        }
     }
 
     /**
@@ -145,7 +174,7 @@ public class Config {
 
     public List<String> getPossibleSteamFolders()
             throws CharConversionException,
-                   InfinitiveLoopException {
+            InfinitiveLoopException {
         String folderList = ini.getKeyValue("/", CONFIG_STEAM_FOLDERS, CONFIG_STEAM_FOLDERS__POSSIBLE_DIRS);
         String[] patterns = folderList.split(Matcher.quoteReplacement(";"), 0);
         List<String> allSteamPaths = new ArrayList<>(80);
@@ -154,7 +183,7 @@ public class Config {
         for (int nRoot = 0; nRoot < roots.length; nRoot++) {
             driveLetters[nRoot] = roots[nRoot].getAbsolutePath();
         }
-        String varDriveLetter = /*Matcher.quoteReplacement(*/ "{drive_letter}"/*)*/;
+        String varDriveLetter = /* Matcher.quoteReplacement( */ "{drive_letter}"/* ) */;
         for (String basePattern : patterns) {
             for (String driveLetter : driveLetters) {
                 allSteamPaths.add(basePattern.replace(varDriveLetter, driveLetter));
@@ -166,43 +195,43 @@ public class Config {
 
     public boolean getCheckForUpdatesAtStartup()
             throws CharConversionException,
-                   InfinitiveLoopException {
+            InfinitiveLoopException {
         return Boolean.parseBoolean(ini.getKeyValue("false", CONFIG_MISC, CONFIG_MISC__CHK_FOR_UPT_AT_STATUP));
     }
 
     public String getLatestSteamFolder()
             throws CharConversionException,
-                   InfinitiveLoopException {
+            InfinitiveLoopException {
         return ini.getKeyValue("", CONFIG_STEAM_FOLDERS, CONFIG_STEAM_FOLDERS__LATEST_DIR);
     }
 
     public int getMaDepth()
             throws CharConversionException,
-                   InfinitiveLoopException {
+            InfinitiveLoopException {
         return Integer.parseInt(ini.getKeyValue("", CONFIG_STEAM_FOLDERS, CONFIG_STEAM_FOLDERS__MAX_DEPTH));
     }
 
     public int getUILatestWidth()
             throws CharConversionException,
-                   InfinitiveLoopException {
+            InfinitiveLoopException {
         return Integer.parseInt(ini.getKeyValue("750", CONFIG_MAIN_WINDOW_UI, CONFIG_MAIN_WINDOW_UI__SIZE_X));
     }
 
     public int getUILatestHeight()
             throws CharConversionException,
-                   InfinitiveLoopException {
+            InfinitiveLoopException {
         return Integer.parseInt(ini.getKeyValue("500", CONFIG_MAIN_WINDOW_UI, CONFIG_MAIN_WINDOW_UI__SIZE_Y));
     }
 
     public int getUIState()
             throws CharConversionException,
-                   InfinitiveLoopException {
+            InfinitiveLoopException {
         return Integer.parseInt(ini.getKeyValue("0", CONFIG_MAIN_WINDOW_UI, CONFIG_MAIN_WINDOW_UI__STATE));
     }
 
     public List<Redist> getRedistFilePatternsAndDesc(boolean includeExp)
             throws CharConversionException,
-                   InfinitiveLoopException {
+            InfinitiveLoopException {
         List<Redist> res = getRedistPatternsAndDesc(CONFIG_REDIST_PATTERNS__FILE_PATTERNS);
         if (includeExp) {
             List<Redist> resAdd = getRedistPatternsAndDesc(CONFIG_REDIST_PATTERNS__FILE_PATTERNS_EXP);
@@ -213,7 +242,7 @@ public class Config {
 
     public List<Redist> getRedistFolderPatternsAndDesc(boolean includeExp)
             throws CharConversionException,
-                   InfinitiveLoopException {
+            InfinitiveLoopException {
         List<Redist> res = getRedistPatternsAndDesc(CONFIG_REDIST_PATTERNS__FOLDER_PATTERNS);
         if (includeExp) {
             List<Redist> resAdd = getRedistPatternsAndDesc(CONFIG_REDIST_PATTERNS__FOLDER_PATTERNS_EXP);
@@ -224,13 +253,13 @@ public class Config {
 
     public boolean getEnableExperimentalPatterns()
             throws CharConversionException,
-                   InfinitiveLoopException {
+            InfinitiveLoopException {
         return Boolean.parseBoolean(ini.getKeyValue("false", CONFIG_REDIST_PATTERNS, CONFIG_REDIST_PATTERNS__ENABLE_EXP_PATTERNS));
     }
 
     private List<Redist> getRedistPatternsAndDesc(String configKey)
             throws CharConversionException,
-                   InfinitiveLoopException {
+            InfinitiveLoopException {
         List<Redist> redistList = new ArrayList<>(8);
         String[] redistTokens = ini.getKeyValue("", CONFIG_REDIST_PATTERNS, configKey).split("\"", 0);
         boolean FileDescToggle = true;
@@ -287,15 +316,15 @@ public class Config {
 
     public String getSelectedLanguage()
             throws CharConversionException,
-                   InfinitiveLoopException,
-                   IOException {
+            InfinitiveLoopException,
+            IOException {
         return getSelectedLanguage(Translation.getAvailLangList());
     }
 
     public String getSelectedLanguage(List<CountryLanguage> availLang)
             throws CharConversionException,
-                   InfinitiveLoopException,
-                   IOException {
+            InfinitiveLoopException,
+            IOException {
         String lang = ini.getKeyValue("", CONFIG_LANG, CONFIG_LANG__SELECTION);
         if (fr.tikione.ini.util.StringHelper.strIsEmpty(lang)) {
             lang = Translation.getSystemLangIfAvailable(availLang);
@@ -310,7 +339,7 @@ public class Config {
 
     public String getLatestVersionUrl()
             throws CharConversionException,
-                   InfinitiveLoopException {
+            InfinitiveLoopException {
         // return ini.getKeyValue("?", CONFIG_UPDATECENTER, CONFIG_UPDATECENTER__LATEST_VERSION_URL);
         return "http://sourceforge.net/projects/tikione/files/tikione-steam-cleaner/uc/steamcleaner_latestversion.txt/download";
     }
