2d1998487cd80174629b2868032be473072c6310
jonathanlermitage <jonathan.lermitage@gmail.com>
2015-03-23 23:21:15 +0100

    removed dead code

--- src/fr/tikione/steam/cleaner/util/FileComparator.java
+++ src/fr/tikione/steam/cleaner/util/FileComparator.java
@@ -1,8 +1,6 @@
 package fr.tikione.steam.cleaner.util;
 
-import static fr.tikione.steam.cleaner.util.FileUtils.checkFile;
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -34,42 +32,14 @@ public class FileComparator {
 			int startIdx = 0;
 			int endIdx = nbfiles - 1;
 			Log.info("debug: ThreadedFileComparator >> startIdx=" + startIdx + ", endIdx=" + endIdx);
-			FileComparatorWorker swThread = new FileComparatorWorker(files, startIdx, endIdx, redistsPatterns, onFiles);
-			swThread.start();
-			swThread.join();
-			List<Redist> res = swThread.checkedFiles;
-			checkedFiles.addAll(res);
-		}
-	}
+//			FileComparatorWorker swThread = new FileComparatorWorker(files, startIdx, endIdx, redistsPatterns, onFiles);
+//			swThread.start();
+//			swThread.join();
+//			List<Redist> res = swThread.checkedFiles;
+//			checkedFiles.addAll(res);
 
-	class FileComparatorWorker extends Thread {
-
-		/** List of files to exam (warning: this list in unmodifiable). */
-		private final List<File> files;
-
-		private final int startIdx;
-
-		private final int endIdx;
-
-		private final List<Redist> redistsPatterns;
-
-		public final List<Redist> checkedFiles;
-
-		private final boolean onFiles;
-
-		FileComparatorWorker(List<File> files, int startIdx, int endIdx, List<Redist> redistsPatterns, boolean onFiles) {
-			this.files = files;
-			this.startIdx = startIdx;
-			this.endIdx = endIdx;
-			this.redistsPatterns = redistsPatterns;
-			this.checkedFiles = new ArrayList<>(20);
-			this.onFiles = onFiles;
-		}
-
-		@Override
-		public void run() {
 			for (int pos = startIdx; pos <= endIdx; pos++) {
-				Redist candidate = checkFile(files.get(pos), redistsPatterns);
+				Redist candidate = FileUtils.checkFile(files.get(pos), redistsPatterns);
 				if (candidate != null) {
 					if (onFiles ? candidate.getFile().isFile() : candidate.getFile().isDirectory()) {
 						if (!checkedFiles.contains(candidate)) {
@@ -80,4 +50,43 @@ public class FileComparator {
 			}
 		}
 	}
+
+//	class FileComparatorWorker extends Thread {
+//
+//		/** List of files to exam (warning: this list in unmodifiable). */
+//		private final List<File> files;
+//
+//		private final int startIdx;
+//
+//		private final int endIdx;
+//
+//		private final List<Redist> redistsPatterns;
+//
+//		public final List<Redist> checkedFiles;
+//
+//		private final boolean onFiles;
+//
+//		FileComparatorWorker(List<File> files, int startIdx, int endIdx, List<Redist> redistsPatterns, boolean onFiles) {
+//			this.files = files;
+//			this.startIdx = startIdx;
+//			this.endIdx = endIdx;
+//			this.redistsPatterns = redistsPatterns;
+//			this.checkedFiles = new ArrayList<>(20);
+//			this.onFiles = onFiles;
+//		}
+//
+//		@Override
+//		public void run() {
+//			for (int pos = startIdx; pos <= endIdx; pos++) {
+//				Redist candidate = checkFile(files.get(pos), redistsPatterns);
+//				if (candidate != null) {
+//					if (onFiles ? candidate.getFile().isFile() : candidate.getFile().isDirectory()) {
+//						if (!checkedFiles.contains(candidate)) {
+//							checkedFiles.add(candidate);
+//						}
+//					}
+//				}
+//			}
+//		}
+//	}
 }
