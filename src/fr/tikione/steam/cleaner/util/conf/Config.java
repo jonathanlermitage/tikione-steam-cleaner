9956308c8000ec91c2f155eec84ace6b13045eeb
jonathanlermitage <jonathan.lermitage@gmail.com>
2015-03-23 20:46:08 +0100

    code cleanup

--- src/fr/tikione/steam/cleaner/util/conf/Config.java
+++ src/fr/tikione/steam/cleaner/util/conf/Config.java
@@ -5,7 +5,6 @@ import fr.tikione.ini.Ini;
 import fr.tikione.steam.cleaner.Main;
 import fr.tikione.steam.cleaner.util.CountryLanguage;
 import fr.tikione.steam.cleaner.util.Log;
-import fr.tikione.steam.cleaner.util.Redist;
 import fr.tikione.steam.cleaner.util.Translation;
 import java.io.CharConversionException;
 import java.io.File;
@@ -105,41 +104,6 @@ public class Config {
         ini.getConfig().enableParseLineConcat(true);
         ini.getConfig().enableReadUnicodeEscConv(true);
         ini.load(configFile, Main.CONF_ENCODING);
-
-//        // Patch for 2.3.1 revision: migrate previous profile to new one.
-//        try {
-//            File configFileP231 = new File(userprofile.getAbsolutePath() + "/tikione-steam-cleaner_config.ini");
-//            if (configFileP231.exists()) {
-//                Ini iniP231 = new Ini();
-//                iniP231.getConfig().enableParseLineConcat(true);
-//                iniP231.getConfig().enableReadUnicodeEscConv(true);
-//                iniP231.load(configFileP231, Main.CONF_ENCODING);
-//                ini.setKeyValue(
-//                        CONFIG_STEAM_FOLDERS, CONFIG_STEAM_FOLDERS__LATEST_DIR,
-//                        iniP231.getKeyValue(CONFIG_STEAM_FOLDERS, CONFIG_STEAM_FOLDERS__LATEST_DIR));
-//                ini.setKeyValue(
-//                        CONFIG_STEAM_FOLDERS, CONFIG_STEAM_FOLDERS__MAX_DEPTH,
-//                        iniP231.getKeyValue(CONFIG_STEAM_FOLDERS, CONFIG_STEAM_FOLDERS__MAX_DEPTH));
-//                ini.setKeyValue(
-//                        CONFIG_LANG, CONFIG_LANG__SELECTION,
-//                        iniP231.getKeyValue(CONFIG_LANG, CONFIG_LANG__SELECTION));
-//                ini.setKeyValue(
-//                        CONFIG_MAIN_WINDOW_UI, CONFIG_MAIN_WINDOW_UI__SIZE_X,
-//                        iniP231.getKeyValue(CONFIG_MAIN_WINDOW_UI, CONFIG_MAIN_WINDOW_UI__SIZE_X));
-//                ini.setKeyValue(
-//                        CONFIG_MAIN_WINDOW_UI, CONFIG_MAIN_WINDOW_UI__SIZE_Y,
-//                        iniP231.getKeyValue(CONFIG_MAIN_WINDOW_UI, CONFIG_MAIN_WINDOW_UI__SIZE_Y));
-//                ini.setKeyValue(
-//                        CONFIG_MAIN_WINDOW_UI, CONFIG_MAIN_WINDOW_UI__STATE,
-//                        iniP231.getKeyValue(CONFIG_MAIN_WINDOW_UI, CONFIG_MAIN_WINDOW_UI__STATE));
-//                ini.setKeyValue(
-//                        CONFIG_MISC, CONFIG_MISC__CHK_FOR_UPT_AT_STATUP,
-//                        iniP231.getKeyValue(CONFIG_MISC, CONFIG_MISC__CHK_FOR_UPT_AT_STATUP));
-//                configFileP231.delete();
-//            }
-//        } catch (InfinitiveLoopException | IOException | IllegalArgumentException ex) {
-//            Log.error(ex);
-//        }
     }
 
     /**
