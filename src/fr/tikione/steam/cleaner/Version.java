a68f400dc9ed24613fd5d81d816f21df15826942
jonathanlermitage <jonathan.lermitage@gmail.com>
2015-06-22 21:09:26 +0200

    prepare next release

--- src/fr/tikione/steam/cleaner/Version.java
+++ src/fr/tikione/steam/cleaner/Version.java
@@ -6,7 +6,7 @@ public class Version {
 
     public static final int MINOR = 4;
 
-    public static final int PATCH = 4;
+    public static final int PATCH = 5;
 
     public static final String VERSION = MAJOR + "." + MINOR + "." + PATCH;
 
