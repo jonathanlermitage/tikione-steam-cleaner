1bb624457e16f75602132342b6786f26e1982b25
jonathanlermitage <jonathan.lermitage@gmail.com>
2015-08-19 20:13:35 +0200

    prepare next version (Kickstarter ad will be removed and Patreon moved somewhere...)

--- src/fr/tikione/steam/cleaner/Version.java
+++ src/fr/tikione/steam/cleaner/Version.java
@@ -4,7 +4,7 @@ public class Version {
 
 	public static final int MAJOR = 2;
 	public static final int MINOR = 9;
-	public static final int PATCH = 0;
+	public static final int PATCH = 1;
 	public static final String VERSION = MAJOR + "." + MINOR + "." + PATCH;
 
 	private Version() {
