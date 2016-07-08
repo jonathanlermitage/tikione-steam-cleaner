adbf5ce6e475c6e795d3d6910065d34651ddc030
jonathan.lermitage@gmail.com <jonathan.lermitage@gmail.com>
2016-07-09 00:52:10 +0200

    change version

--- src/main/java/fr/tikione/steam/cleaner/Version.java
+++ src/main/java/fr/tikione/steam/cleaner/Version.java
@@ -4,7 +4,7 @@ public class Version {
 
 	public static final int MAJOR = 3;
 	public static final int MINOR = 0;
-	public static final int PATCH = 0;
+	public static final int PATCH = 1;
 	public static final String VERSION = MAJOR + "." + MINOR + "." + PATCH;
 
 	private Version() {
