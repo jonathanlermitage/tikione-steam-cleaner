93e3454456973a4744bbec7feee630b6afb63cc7
jonathanlermitage <jonathan.lermitage@gmail.com>
2017-05-13 17:04:50 +0200

    add missing launcher to start application with debug flag

---
+++ dist2/tikione-steam-cleaner-portable-debug.bat
@@ -0,0 +1,21 @@
+@echo off
+rem ---------------------------------------------------------------------------------------------------------------------------
+rem TikiOne Steam Cleaner start-up script.
+rem Used to launch TikiOne Steam Cleaner with the bundled JVM (if exists) or the operating system's JVM.
+rem ---------------------------------------------------------------------------------------------------------------------------
+
+cd %~dp0%
+
+set "OPATH=%PATH%"
+set "PATH=%cd%\jre\bin\;%~dp0%\jre\bin\"
+set "PATH=%PATH%;C:\Program Files (x86)\Java\jre8\bin\"
+set "PATH=%PATH%;C:\Program Files (x86)\Java\jre9\bin\"
+set "PATH=%PATH%;C:\Program Files (x86)\Java\jre10\bin\"
+set "PATH=%PATH%;C:\Program Files (x86)\Java\jre\bin\"
+set "PATH=%PATH%;C:\Program Files\Java\jre8\bin\"
+set "PATH=%PATH%;C:\Program Files\Java\jre9\bin\"
+set "PATH=%PATH%;C:\Program Files\Java\jre10\bin\"
+set "PATH=%PATH%;C:\Program Files\Java\jre\bin\"
+set "PATH=%PATH%;%OPATH%"
+
+start "" "javaw.exe" "-jar" "-splash:tikione-steam-cleaner.png" "-Xms32m" "-Xmx512m" "-XX:-HeapDumpOnOutOfMemoryError" "-XX:HeapDumpPath=crashdump.hprof" "tikione-steam-cleaner.jar" "enablePortablemode"
